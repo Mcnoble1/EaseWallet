73dd61301e4c39f46e0c6b57d6b8b3aa59e9453c
mcnoble1 <idowufestustemiloluwa@gmail.com>
2024-09-16 03:29:00 +0100

    feat: updates :octocat:

--- src/components/ReviewsList.tsx
+++ src/components/ReviewsList.tsx
@@ -33,13 +33,11 @@ const ReviewsList: React.FC<ReviewsListProps> = ({ reviews }) => {
         <p className="text-gray-10">No reviews yet. Be the first to leave one!</p>
       ) : (
         <div>
-          {/* Display the average rating */}
           <div className="flex items-center justify-center mb-6">
             <span className="text-yellow text-2xl font-semibold">{averageRating.toFixed(1)}</span>
             <span className="text-gray text-2xl ml-2">/ 5</span>
           </div>
 
-          {/* Render the list of reviews */}
           {reviews?.map((review, index) => (
             <div key={index} className="bg-tertiary p-4 mb-4 rounded-lg shadow-md">
               <div className="flex items-center mb-4">
@@ -61,7 +59,6 @@ const ReviewsList: React.FC<ReviewsListProps> = ({ reviews }) => {
                   <span key={i} className="text-gray-200 text-2xl">★</span>
                 ))}
               </div>
-              {/* Review Text */}
               <p className="text-white">{review.review}</p>
             </div>
           ))}
